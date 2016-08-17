19a1128008d233a465efbfd971383d2be67b9865
Da_Macchi <damacchi91@gmail.com>
2016-08-17 15:24:38 +0200

    Alpha 17/08/16
    Ho sistematolo sparo. Ora la meccanica è basata su de trigger.
    Probabilmente funzionerà al meglio quando ci saranno tutti e 4 i colori.

--- Roboss.ino
+++ Roboss.ino
@@ -10,40 +10,72 @@
 Pixy pixy;
 int red = 6;
 int white = 5;
-uint8_t blocks;
 int i;
+int color;
+long timeStart;
+
+//triggers
+bool newAttack = true;
+bool gameOver = false;
+bool attackWon = false;
+bool attackEND = false;
+
+//timers
+int fireTimeDelta;
+
+void GameOver() {
+  digitalWrite(red, HIGH);
+  digitalWrite(white, HIGH);
+  delay(1000);
+  digitalWrite(red, LOW);
+  digitalWrite(white, LOW);
+  delay(1000);
+}
+
+void AttackWon() {
+  Serial.print("catched \n \n");
+  digitalWrite(red, LOW);
+  digitalWrite(white, LOW);
+  attackWon = false;
+  attackEND = false;
+  newAttack = true;
+  delay(4000);
+}
 
 //random color fire attack
 void Fire() {
-  if (random(2) == 1) {
-    Serial.print("red fire \n");
+  if (newAttack == true) {
+    Serial.print("FIRE! \n");
+    timeStart = millis();
+    color = random(2);
+    newAttack = false;
+  }
+  if (color == 1) {
     digitalWrite(red, HIGH);
-    delay(3000); //time for the player to rise the shield
     CatchFire(1, 2);
-    digitalWrite(red, LOW);
-    delay(3000);
   } else {
-    Serial.print("white fire \n");
     digitalWrite(white, HIGH);
-    delay(3000);
     CatchFire(2, 1);
-    digitalWrite(white, LOW);
-    delay(3000);
+  }
+  if (millis() - timeStart > 3000) {
+    attackEND = true;
+    if (attackWon == false) {
+      gameOver = true;
+    }
   }
 }
 
 //check if the player rised the correct shield and hid the other one
 void CatchFire(int signature, int oppositeSig) {
+  uint16_t blocks;
   blocks = pixy.getBlocks();
   if (blocks) {
     if (blocks == 1) {
-      Serial.print("un blocco");
       if (pixy.blocks[0].signature == signature) {
-        Serial.print("WIN \n \n");
-      } else Serial.print("LOSE \n \n");
+        attackWon = true;
+      } else attackWon = false;
     }
     if (blocks > 1) {
-      Serial.print("più blocchi");
       bool win = false;
       for (i = 0; i < blocks; i++) {
         if (pixy.blocks[i].signature == signature) {
@@ -55,10 +87,10 @@ void CatchFire(int signature, int oppositeSig) {
         }
       }
       if (win == true) {
-        Serial.print("WIN \n \n");
-      } else Serial.print("LOSE \n \n");
+        attackWon = true;
+      } else attackWon = false;
     }
-  } else Serial.print("LOSE no blocchi \n \n");
+  } 
 }
 
 void setup() {
@@ -75,6 +107,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Fire();
-
+  if (gameOver == true) {
+    GameOver();
+  } else {
+    Fire();
+    if (attackEND == true && attackWon == true) {
+      AttackWon();
+    }
+  }
 }
